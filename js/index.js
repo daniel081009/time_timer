69e3b5e0eabbeb19abba9068a9122415e3c7b59d
오단휘 <dan@odanhwiui-MacBookPro.local>
2023-07-27 23:43:36 +0900

    end

--- js/index.js
+++ js/index.js
@@ -24,20 +24,16 @@ var run_lock_t = 1;
 
 
 var options = {
-    size: el.clientHeight
+    size:document.body.clientWidth/2.1
 }
 
-var color = 'rgba(255,255,255,.5)';
+var color = '#f90000';
 
 var radius = options.size / 2;
 var rad = 0;
 
 var stp = 0;
 var toPi = Math.PI / 180;
-
-
-
-
 
 var canvas = document.createElement('canvas');
 canvas.id = "canvas";
@@ -60,32 +56,23 @@ function dingeling (){
   x.play();
 }
 
-
-
 function overOut() {
-
     if (rad <= radius){
-
         ctx.clearRect(
             -1/2 * canvas.width, 
             -1/2 * canvas.height, 
             canvas.width*2, 
             canvas.height*2
         );
-        
         ctx.beginPath();
         ctx.moveTo(0, 0);
         ctx.arc(0, 0, rad, 0, stp, false);
         ctx.arc(0, 0, radius, stp, 0, true);
         ctx.closePath();
-
         ctx.fillStyle = color;
         ctx.fill();
-
-        rad += 2;
-        
+        rad -= 2;
         interout = setTimeout(overOut, 10);
-
     } else {
 
         ctx.clearRect(
@@ -99,154 +86,123 @@ function overOut() {
 
     }
 }
-
 
 
 function fadeOut() {
-
-    if (rad <= radius){
-
-        ctx.clearRect(
-            -1/2 * canvas.width, 
-            -1/2 * canvas.height, 
-            canvas.width*2, 
-            canvas.height*2
-        );
-        
+    if (run_lock_d == 1) {
         ctx.beginPath();
-        ctx.arc(0, 0, rad, 0, 2 * Math.PI, false);
-        ctx.arc(0, 0, radius, 0, 2 * Math.PI, true);
+        ctx.arc(0, 0, radius, -0.5 * Math.PI, (-0.5 + 2 ) * Math.PI);
         ctx.closePath();
-
-        ctx.fillStyle = color;
         ctx.fill();
-
-        rad += 2;
-
-        interout = setTimeout(fadeOut, 10);
-
-    } else {
-
-        ctx.clearRect(
-            -1/2 * canvas.width, 
-            -1/2 * canvas.height, 
-            canvas.width*2, 
-            canvas.height*2
-        );
-
-        clearTimeout(interout);
-
+        interout = setTimeout(fadeOut, 50);
     }
 }
+// function drawCircle() {
+//     ctx.clearRect(-0.5 * canvas.width, -0.5 * canvas.height, canvas.width, canvas.height);
+//     ctx.beginPath();
+//     // 시작 각도 변경
+//     ctx.arc(0, 0, radius, 0, -1*stp);
+//     console.log(stp);
+//     ctx.strokeStyle = 'rgba(255, 0, 0, 1)';
+//     ctx.lineWidth = 68;
+//     ctx.stroke();
+//     ctx.closePath();
+//     ctx.rotate((Math.PI / 180) * 90);
+//     ctx.font = 'bold 48px sans-serif';
+//     ctx.fillStyle = 'white';
+//     ctx.textAlign = 'center';
+//     ctx.textBaseline = 'middle';
+//     ctx.fillText(ftime - ttime, 0, 0);
+// }
 
-
-
-function drawCircle (){
-
-    ctx.clearRect(
-        -0.5 * canvas.width, 
-        -0.5 * canvas.height, 
-        canvas.width*2, 
-        canvas.height*2
-    );
-
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.arc(0, 0, radius, 0, stp);
-    ctx.closePath();
-    ctx.fillStyle = color;
-    ctx.fill();
-
-};
-
-
-
-function drawTime (){
-
-    if (run_lock_d == 0){
-
-        if (ttime < ftime){
-            ttime ++;
-            stp = ntime * ttime * toPi;
+// function drawTime (){
+//     if (run_lock_d == 0){
+//         if (ttime < ftime){
+//             ttime++;
+//             stp = 2 * Math.PI - ntime * ttime * toPi; // 변경된 부분
+//             drawCircle();
+//         } else {
+//             run_lock_d = 1;
+//             ttime = 0;
+//             ntime = 0;
+//             stop.style.display = 'none';
+//             start.style.display = 'block';
+//             interout = setTimeout(fadeOut, 200);
+//             dingeling();
+//             mn.disabled = false;
+//             sc.disabled = false;
+//         }
+//     }
+// }
+function drawTime() {
+    if (run_lock_d == 0) {
+        if (ttime < ftime) {
+            ttime++;
+            stp = 2 * Math.PI - ntime * ttime * toPi;
             drawCircle();
-
         } else {
-            
             run_lock_d = 1;
-
             ttime = 0;
             ntime = 0;
-
             stop.style.display = 'none';
             start.style.display = 'block';
-
-            interout = setTimeout(fadeOut, 200);
-            dingeling();
-
-            mn.disabled = false;
-            sc.disabled = false;
-
         }
     }
 }
+  
+function drawCircle() {
+    ctx.clearRect(-0.5 * canvas.width, -0.5 * canvas.height, canvas.width, canvas.height);
+    ctx.save();
+    ctx.rotate((Math.PI / 180) * 90);
+    ctx.font = 'bold 86px sans-serif';
+    ctx.fillStyle = 'white';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+  
+    let timeString = `${minutes}:${secondes}`;
+    ctx.fillText(timeString, 0, 0);
 
-
-
-
+    ctx.font = 'bold 48px sans-serif';
+    let msg = document.getElementById("mission")
+    ctx.fillText(msg.value, 0, 100)
+  
+    ctx.restore();
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 1 * stp);
+    ctx.strokeStyle = 'rgba(255, 0, 0, 1)';
+    ctx.lineWidth = 128;
+    ctx.stroke();
+    ctx.closePath();
+  }
 
 function changeTime(){
-
     if (run_lock_t == 0){
-
         if ((ctime % 100) == 0){
-
             if (secondes > 0){
-
                 secondes --;
-                
                 if (sc.value < 10){
-
                     sc.value = '0' + secondes;
-    
                 } else {
-
                     sc.value = secondes;
-        
                 }
-
             } else if (minutes > 0) {
-
                 minutes --;
-                
                 if (mn.value < 10){
-
                     mn.value = '0' + minutes;
-
                 } else {
-                    
                     mn.value = minutes;
                 }
-
                 secondes = 59;
                 sc.value = secondes;
-
             } else {
-
                 sc.value = '00';
-                
                 run_lock_t = 1;
-
             }
-
             ctime = 0;
         }
-
         ctime ++;
     }
-
 }
-
-
 
 function init (){
 
@@ -274,8 +230,6 @@ function init (){
     rad = 0;
 }
 
-
-
 function over (){
             
     run_lock_d = 1;
@@ -298,8 +252,6 @@ function over (){
 
 }
 
-
-
 function pause (){
 
     stop.style.display = 'none';
@@ -310,8 +262,6 @@ function pause (){
     
 }
 
-
-
 function replay (){
 
     unstop.style.display = 'none';
@@ -321,8 +271,6 @@ function replay (){
     run_lock_t = 0;
 
 }
-
-
 
 function play (){
 
@@ -339,11 +287,8 @@ function play (){
 
 }
 
-
-
 interTime = setInterval(changeTime, 10);
 interval = setInterval(drawTime, 10);
-
 
 function zeroFirst (e){
 
@@ -355,4 +300,3 @@ function zeroFirst (e){
 
 sc.addEventListener('input', zeroFirst);
 mn.addEventListener('input', zeroFirst);
-
